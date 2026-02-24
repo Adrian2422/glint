b83b679ebbbd2876e66b2706b17f8ae8fc25ca9d
Adrian Kloc <adrian.kloc47@gmail.com>
2026-02-24 18:26:54 +0100

    feat: zenstack implementation and basic usage

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
@@ -8,29 +7,29 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(globalPrefix);
-  const openApiDoc = SwaggerModule.createDocument(
-    app,
-    new DocumentBuilder()
-      .setTitle('Glint')
-      .setDescription('The Glint api documentation')
-      .setExternalDoc(
-        'swagger.json',
-        `http://localhost:${port}/${globalPrefix}/swagger.json`,
-      )
-      .setVersion('1.0')
-      .build(),
-  );
-
-  SwaggerModule.setup('api/swagger', app, openApiDoc, {
-    jsonDocumentUrl: '/api/swagger.json',
-  });
+  // const openApiDoc = SwaggerModule.createDocument(
+  //   app,
+  //   new DocumentBuilder()
+  //     .setTitle('Glint')
+  //     .setDescription('The Glint api documentation')
+  //     .setExternalDoc(
+  //       'swagger.json',
+  //       `http://localhost:${port}/${globalPrefix}/swagger.json`,
+  //     )
+  //     .setVersion('1.0')
+  //     .build(),
+  // );
+  //
+  // SwaggerModule.setup('api/swagger', app, openApiDoc, {
+  //   jsonDocumentUrl: '/api/swagger.json',
+  // });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
-  Logger.log(
-    `📑 Swagger is running on: http://localhost:${port}/${globalPrefix}/swagger`,
-  );
+  // Logger.log(
+  //   `📑 Swagger is running on: http://localhost:${port}/${globalPrefix}/swagger`,
+  // );
 }
 
 void bootstrap();
